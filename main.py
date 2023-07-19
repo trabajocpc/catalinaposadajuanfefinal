fe1633b437ff3269da4a8ca2bf8d3dc8dae20634
207 <207@academicos.cesde.edu.co>
2023-07-19 17:54:21 -0500

    ok

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from data.platos import platosPopulares
+from helpers.lanzarTabla import lanzarTablaHTML
+tablaPlatos=pd.DataFrame(platosPopulares)
+lanzarTablaHTML(tablaPlatos,"tabla1")
